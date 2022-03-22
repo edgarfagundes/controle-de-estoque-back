a164faa88f938e1012aa58dac19f1010aac66bac
edgarfagundes <edgarfagundessss@gmail.com>
2022-03-22 06:15:18 -0300

    Primeiro commit

---
+++ src/main/java/com/example/cotroladordeestoque/CotroladorDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.cotroladordeestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotroladorDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotroladorDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cotroladordeestoque/CotroladorDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cotroladordeestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotroladorDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
